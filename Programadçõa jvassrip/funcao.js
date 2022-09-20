0a1e60c50693f9bc1b329385850ba940e7e83361
Leonardovianaaquino <113393003+Leonardovianaaquino@users.noreply.github.com>
2022-09-19 21:28:33 -0300

    Add files via upload

---
+++ Programadçõa jvassrip/funcao.js
@@ -0,0 +1,23 @@
+
+ano = prompt("Informe o ano de Nascimento")
+nome = prompt("Informe seu Nome")
+atual = 2022
+idade = atual-ano
+console.log(" Sr(a)",nome ," ,a sua idade é ",atual-ano,"anos.")
+if(idade>=18){
+    alert("Você é maior de idade")
+}
+else{
+    alert("Você é menor de idade")
+}
+
+
+
+
+
+
+n1=prompt("Infome um Valor")
+n2=prompt("Informe outro Valor")
+
+soma=parseInt(n1)+ parseInt(n2)
+alert("O resultado foi "+ soma)
---
+++ termometro/termometro.js
@@ -0,0 +1,8 @@
+temperatura= prompt("Informe sua temperatura")
+if(temperatura>=38){
+    alert("Você esta com febre " + temperatura)
+}
+if(temperatura<=37.99){
+    alert("Nada de febre "+ temperatura)
+}
+
