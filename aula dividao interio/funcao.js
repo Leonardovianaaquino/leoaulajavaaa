3126ec17d232e61fc43c747d3dce11a7b62499db
Leonardovianaaquino <113393003+Leonardovianaaquino@users.noreply.github.com>
2022-09-28 20:59:24 -0300

    Add files via upload

---
+++ aula dividao interio/funcao.js
@@ -0,0 +1,10 @@
+n1=parseInt(prompt("Digite o Primeiro Valor"))
+n2=parseInt(prompt("Digite o Segundo Valor "))
+
+while(n2==0){
+    alert("Digite Outro valor")
+    n2=parseInt(prompt("Digite o Segundo Valor "))
+}
+
+   divisao=n1/n2
+   alert(divisao)
---
+++ auula html e java inteiro 2/funcao.js
@@ -0,0 +1,9 @@
+function soma(){
+   
+    s = parseInt( document.form1.num1.value) + parseInt( document.form1.num2.value)+ parseInt(document.form1.num3.value)
+    alert(s)
+    }
+function imc(){
+    imc = documen.form1.peso.value/ (document.fomr1.alt.value*document.form1.alt.value)
+    alert(imc)
+}
